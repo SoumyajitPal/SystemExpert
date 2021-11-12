8603969672ffa8bde0eb48f4afd891339ef93464
Soumyajit Pal ⚡ <sopal@microsoft.com>
2021-11-12 23:54:30 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs');
+
+const DATA_DIR = 'aedb_data';
+
+const app = express();
+app.use(express.json());
+
+const hashtable = {};
+
+app.post('/memory/:key', (req, res) => {
+    hashtable;e[req.params.key] = req.body.data;
+    res.send();
+});
+
+app.get('/memory/:key', (req, res) => {
+    const key = req.params.key;
+    if (key in hashtable) {
+        res.send(hashtable[key]);
+        return;
+    }
+});
+
+app.post('/disk/:key', (req, res) => {
+    const destinationFile = `${DATA_DIR}/${req.params.key}`;
+    fs.writeFileSync(destinationFile, req.body.data);
+    res.send();
+});
+
+app.get('/disk/:key', (req, res) => {
+    const destinationFile = `${DATA_DIR}/${req.params.key}`;
+
+    try {
+        const data = fs.readFileSync(destinationFile);
+    } catch (e) {
+        res.send('null');
+    }
+});
+
+app.listen(3001, () => {
+    console.log('Listening on port 3001');
+});
